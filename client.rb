67b3fb19820eb1e4ee5da8cc742de02231893964
RJ Robinson <robertj.robinson@gmail.com>
2014-12-13 12:40:07 -0500

    added connection w/ bunny

---
+++ client.rb
@@ -0,0 +1,34 @@
+require 'bunny'
+require 'pry'
+
+
+user = 'admin'
+pass = '1Le6VZsVsSpY'
+host = 'rabbitmq-1.45c4208d-bmoyles0117.node.tutum.io'
+port = 5672
+
+conn = Bunny.new(
+  host:     host,
+  user:     user,
+  password: pass,
+  port:     port
+  )
+
+conn.start
+
+ch  = conn.create_channel
+x   = ch.topic('rjrobinson')
+q   = ch.queue('rjrobinson')
+
+q.bind(x, routing_key: 'formula')
+
+puts " [x] Waiting for data. To exit press CTRL+C"
+
+begin
+  q.subscribe(block: true) do |delivery_info, properties, body|
+    p " [x] #{delivery_info.routing_key}:#{body}"
+  end
+rescue Interrupt => _
+  ch.close
+  conn.close
+end
