0860ed33597f1206525cb882608562a53226de36
RJ Robinson <robertj.robinson@gmail.com>
2014-12-15 12:31:01 -0500

    added config.ru file

--- eval_scripts/eval3.rb
+++ eval_scripts/eval3.rb
@@ -3,19 +3,19 @@ beginning_time = Time.now
 
 mathstring = '123 + 76 - 8 - 180 + 34'
 
-# def get_answer(string)
-#   elements = string.split(' ')
-#   answer = elements.shift.to_i
-#
-#   while elements.length > 0
-#     if elements.shift == '+'
-#       answer += elements.shift.to_i
-#     else
-#       answer -= elements.shift.to_i
-#     end
-#   end
-#   answer
-# end
+def get_answer2(string)
+  elements = string.split(' ')
+  answer = elements.shift.to_i
+
+  while elements.length > 0
+    if elements.shift == '+'
+      answer += elements.shift.to_i
+    else
+      answer -= elements.shift.to_i
+    end
+  end
+  answer
+end
 
 def get_answer(string)
   elements = string.split(' ')
@@ -26,12 +26,7 @@ def get_answer(string)
   end
   answer
 end
-
-
-
 1_000_000.times { get_answer(mathstring) }
-
-
 end_time = Time.now
 puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
 
